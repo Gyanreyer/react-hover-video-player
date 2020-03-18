149027e98319a8229a2e002811ec95a18749aca7
Ryan Geyer <ryan@waymark.com>
2020-03-17 22:28:19 -0400

    Make component render an image and a video element
    TODO
    - Styling
    - Play on hover behavior
    - Tighten up prop validation/handling
    - Write tests

--- demo/src/index.js
+++ demo/src/index.js
@@ -1,15 +1,25 @@
-import React, {Component} from 'react'
-import {render} from 'react-dom'
+import React, { Component } from "react";
+import { render } from "react-dom";
 
-import Example from '../../src'
+import HoverVideoPreview from "../../src";
 
 export default class Demo extends Component {
   render() {
-    return <div>
-      <h1>react-hover-video-preview Demo</h1>
-      <Example/>
-    </div>
+    return (
+      <div>
+        <h1>react-hover-video-preview Demo</h1>
+        <HoverVideoPreview
+          imageSrc="https://via.placeholder.com/150"
+          videoSources={[
+            {
+              src:
+                "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4"
+            }
+          ]}
+        />
+      </div>
+    );
   }
 }
 
-render(<Demo/>, document.querySelector('#demo'))
+render(<Demo />, document.querySelector("#demo"));
--- src/index.js
+++ src/index.js
@@ -1,9 +1,53 @@
-import React, {Component} from 'react'
+import React from "react";
 
-export default class extends Component {
-  render() {
-    return <div>
-      <h2>Welcome to React components</h2>
-    </div>
+const HoverVideoPreview = ({
+  imageSrc,
+  imageSrcSet,
+  alt = "",
+  videoSrc,
+  videoSources = [],
+  controls = false,
+  loop = true,
+  muted = true,
+  preload = "metadata"
+}) => {
+  const videoSourcesArray = [...videoSources];
+
+  if (videoSrc) {
+    switch (typeof videoSrc) {
+      case "object":
+        videoSourcesArray.splice(0, 0, videoSrc);
+        break;
+      case "string":
+        videoSourcesArray.splice(0, 0, {
+          src: videoSrc
+        });
+        break;
+      default:
+        if (videoSrc) {
+          console.error(
+            `Invalid variable of type ${typeof videoSrc} provided for videoSrc prop. videoSrc must be a string or an object.`
+          );
+        }
+    }
   }
-}
+
+  return (
+    <div>
+      <img src={imageSrc} srcSet={imageSrcSet} alt={alt} />
+      <video
+        controls={controls}
+        loop={loop}
+        muted={muted}
+        playsinline
+        preload={preload}
+      >
+        {videoSourcesArray.map(videoSourceProps => (
+          <source {...videoSourceProps} />
+        ))}
+      </video>
+    </div>
+  );
+};
+
+export default HoverVideoPreview;
