5dc1ed0079aec717d6377c85fe3046c9d0de5a68
Ryan Geyer <ryan@waymark.com>
2020-04-12 15:49:37 -0400

    feat: Second attempt at LoadingOverlay export

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 import HoverVideoPlayer from './HoverVideoPlayer';
+import * as LoadingOverlays from './LoadingOverlays';
 
 export default HoverVideoPlayer;
+
+// LoadingOverlays can be imported at react-hover-video-player/LoadingOverlays
+export { LoadingOverlays };
