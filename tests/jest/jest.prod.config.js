bab24c39c7f17b6c6c025580ab223ac442306abf
Ryan Geyer <ryan@waymark.com>
2021-08-20 21:16:54 -0400

    test: remove jest tests

--- tests/jest/jest.prod.config.js
+++
@@ -1,9 +0,0 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
-const baseConfig = require('./jest.base.config');
-
-module.exports = {
-  ...baseConfig,
-  moduleNameMapper: {
-    'react-hover-video-player': '<rootDir>/dist/index.js',
-  },
-};
