da1045003aea2937dd95f1e6762ef51e32e809c1
Ryan <gyanreyer@gmail.com>
2022-05-28 18:06:47 -0400

    refactor(hovervideoplayer): refactor component logic to rely less heavily on effects

--- src/component/HoverVideoPlayer.tsx
+++ src/component/HoverVideoPlayer.tsx
@@ -1,10 +1,16 @@
-import React, { useRef, useImperativeHandle, useEffect } from 'react';
+import React, {
+  useRef,
+  useImperativeHandle,
+  useEffect,
+  useState,
+  useCallback,
+} from 'react';
 
 import useSetAdditionalAttributesOnVideo from '../hooks/useSetAdditionalAttributesOnVideo';
-import useIsHoveringOverVideo from '../hooks/useIsHoveringOverVideo';
-import useManageVideoPlayback from '../hooks/useManageVideoPlayback';
 import useFormatVideoSrc from '../hooks/useFormatVideoSrc';
 import useFormatVideoCaptions from '../hooks/useFormatVideoCaptions';
+import useHoverTargetElement from '../hooks/useHoverTargetElement';
+import useManageHoverEvents from '../hooks/useManageHoverEvents';
 
 import {
   expandToFillContainerStyle,
@@ -12,7 +18,6 @@ import {
   pausedOverlayWrapperSizingStyles,
   videoSizingStyles,
 } from './HoverVideoPlayer.styles';
-import { OverlayState } from '../constants/OverlayState';
 import { HoverVideoPlayerProps, VideoSource } from '../HoverVideoPlayer.types';
 
 /**
@@ -77,52 +82,14 @@ const HoverVideoPlayer = ({
     disablePictureInPicture
   );
 
-  const isHoveringOverVideo = useIsHoveringOverVideo(
-    // If the hoverTarget prop wasn't provided, fall back to the component's container div
-    hoverTarget || containerRef,
-    disableDefaultEventHandling,
-    onHoverStart,
-    onHoverEnd
-  );
+  // Get the hover target element from the hoverTarget prop, or default to the component's container div
+  const hoverTargetElement = useHoverTargetElement(hoverTarget || containerRef);
 
-  // We should attempt to play the video if the user is hovering over it or the `focused` override prop is enabled
-  const shouldPlayVideo = isHoveringOverVideo || focused;
-
-  const hasPausedOverlay = Boolean(pausedOverlay);
-  const hasHoverOverlay = Boolean(hoverOverlay);
-
-  // If we have a paused or hover overlay, the player should wait
-  // for the overlay(s) to finish transitioning back in before we
-  // pause the video
-  const shouldWaitForOverlayTransitionBeforePausing =
-    hasPausedOverlay || hasHoverOverlay;
-
-  const hasLoadingOverlay = Boolean(loadingOverlay);
-
-  // Effect handles transitioning the video between playing or paused states
-  // depending on the current value for `shouldPlayVideo`
-  const [overlayState, isVideoActive] = useManageVideoPlayback(
-    videoRef,
-    shouldPlayVideo,
-    playbackRangeStart,
-    playbackRangeEnd,
-    loop,
-    restartOnPaused,
-    shouldWaitForOverlayTransitionBeforePausing,
-    hasLoadingOverlay,
-    overlayTransitionDuration,
-    shouldSuppressPlaybackInterruptedErrors
-  );
-
-  // If the video's sources should be unloaded when it's paused and the video is not currently active, we can unload the video's sources.
-  // We will remove the video's <source> tags in this render and then call video.load() in an effect to
-  // fully unload the video
-  const shouldUnloadVideo = unloadVideoOnPaused && !isVideoActive;
+  const nextVideoStartTimeRef = useRef(0);
 
   // Parse the sources and captions into formatted arrays that we can use to
   // render <source> and <track> elements for the video
   const formattedVideoCaptions = useFormatVideoCaptions(videoCaptions);
-
   const formattedVideoSources = useFormatVideoSrc(
     videoSrc,
     playbackRangeStart,
@@ -133,31 +100,165 @@ const HoverVideoPlayer = ({
     formattedVideoSources
   );
 
-  // Effect re-loads the video if its sources should be unloaded when paused or if the videoSrc changed
-  useEffect(() => {
-    // Only reload when the video is paused
-    if (isVideoActive) return;
+  const attemptToPlayVideo = useCallback(async () => {
+    const videoElement = videoRef.current;
 
+    videoElement.currentTime = nextVideoStartTimeRef.current;
     if (
-      unloadVideoOnPaused ||
+      !videoElement.currentSrc ||
       previousFormattedVideoSourcesRef.current !== formattedVideoSources
     ) {
-      // Perform a manual load to unload the video's current source
+      // If the video element doesn't have a loaded source or the source has changed since the
+      // last time we played the video, make sure to force the video to load the most up-to-date sources
+      videoElement.load();
+    }
+
+    await videoElement.play().catch((error: DOMException) => {
+      // If shouldSuppressPlaybackInterruptedErrors is true and this is an AbortError, do nothing instead of logging it.
+      if (
+        shouldSuppressPlaybackInterruptedErrors &&
+        error.name === 'AbortError'
+      ) {
+        return;
+      }
+
+      // Additional handling for when browsers block playback for unmuted videos.
+      // This is unfortunately necessary because most modern browsers do not allow playing videos with audio
+      //  until the user has "interacted" with the page by clicking somewhere at least once; mouseenter events
+      //  don't count.
+      // If the video isn't muted and playback failed with a `NotAllowedError`, this means the browser blocked
+      // playing the video because the user hasn't clicked anywhere on the page yet.
+      if (!videoElement.muted && error.name === 'NotAllowedError') {
+        console.warn(
+          'HoverVideoPlayer: Playback with sound was blocked by the browser. Attempting to play again with the video muted; audio will be restored if the user clicks on the page.'
+        );
+        // Mute the video and attempt to play again
+        videoElement.muted = true;
+        videoElement.play();
+
+        // When the user clicks on the document, unmute the video since we should now
+        // be free to play audio
+        const onClickDocument = () => {
+          videoElement.muted = false;
+
+          // Clean up the event listener so it is only fired once
+          document.removeEventListener('click', onClickDocument);
+        };
+        document.addEventListener('click', onClickDocument);
+      } else {
+        // Log any other playback errors with console.error
+        console.error(`HoverVideoPlayer: ${error.message}`);
+      }
+    });
+  }, [formattedVideoSources, shouldSuppressPlaybackInterruptedErrors]);
+
+  const [playbackState, setPlaybackState] = useState<
+    'PAUSED' | 'LOADING' | 'PLAYING'
+  >('PAUSED');
+
+  const [isHovering, setIsHovering] = useState(false);
+
+  const hasPausedOverlay = Boolean(pausedOverlay);
+  const hasHoverOverlay = Boolean(hoverOverlay);
+
+  // If we have a paused or hover overlay, the player should wait
+  // for the overlay(s) to finish transitioning back in before we
+  // pause the video
+  const shouldWaitForOverlayTransitionBeforePausing =
+    hasPausedOverlay || hasHoverOverlay;
+
+  useEffect(() => {
+    if (!hoverTargetElement) return undefined;
+
+    const attemptToPlayVideoOnHover = async () => {
+      setIsHovering(true);
+
+      attemptToPlayVideo();
+    };
+    const attemptToPauseVideoOnHoverEnd = () => {
+      setIsHovering(false);
+      const videoElement = videoRef.current;
+
+      const pauseVideo = () => {
+        videoElement.pause();
+
+        // Performing post-save cleanup tasks in here rather than the onPause listener
+        // because onPause can also be called when the video reaches the end of a playback range
+        // and it's just simpler to deal with that separately
+        if (restartOnPaused) {
+          videoElement.currentTime = playbackRangeStart || 0;
+        }
+        nextVideoStartTimeRef.current = videoElement.currentTime;
+
+        setPlaybackState('PAUSED');
+      };
+
+      if (shouldWaitForOverlayTransitionBeforePausing) {
+        window.setTimeout(pauseVideo, overlayTransitionDuration);
+      } else {
+        pauseVideo();
+      }
+    };
+
+    hoverTargetElement.addEventListener(
+      'hvp:hoverStart',
+      attemptToPlayVideoOnHover
+    );
+    hoverTargetElement.addEventListener(
+      'hvp:hoverEnd',
+      attemptToPauseVideoOnHoverEnd
+    );
+
+    return () => {
+      hoverTargetElement.removeEventListener(
+        'hvp:hoverStart',
+        attemptToPlayVideoOnHover
+      );
+      hoverTargetElement.removeEventListener(
+        'hvp:hoverEnd',
+        attemptToPauseVideoOnHoverEnd
+      );
+    };
+  }, [
+    attemptToPlayVideo,
+    hoverTargetElement,
+    overlayTransitionDuration,
+    playbackRangeStart,
+    restartOnPaused,
+    shouldWaitForOverlayTransitionBeforePausing,
+  ]);
+
+  useManageHoverEvents(
+    hoverTargetElement,
+    focused,
+    disableDefaultEventHandling,
+    onHoverStart,
+    onHoverEnd
+  );
+
+  // We should attempt to play the video if the user is hovering over it or the `focused` override prop is enabled
+  // const shouldPlayVideo = isHoveringOverVideo || focused;
+
+  const hasLoadingOverlay = Boolean(loadingOverlay);
+
+  // If the video's sources should be unloaded when it's paused and the video is not currently active, we can unload the video's sources.
+  // We will remove the video's <source> tags in this render and then call video.load() in an effect to
+  // fully unload the video
+  const shouldUnloadVideo =
+    unloadVideoOnPaused && !isHovering && playbackState !== 'PLAYING';
+
+  useEffect(() => {
+    // If shouldUnloadVideo is true, this effect is being run after the video's
+    // sources have been removed, so call load on the video to unload any sources it currently has loaded
+    if (shouldUnloadVideo) {
       const videoElement = videoRef.current;
       videoElement.load();
-
-      previousFormattedVideoSourcesRef.current = formattedVideoSources;
     }
-  }, [isVideoActive, unloadVideoOnPaused, formattedVideoSources]);
+  }, [shouldUnloadVideo]);
 
-  const hasPlaybackRange =
-    playbackRangeStart !== null || playbackRangeEnd !== null;
-
-  // Show the paused overlay if the overlay state is either "paused" OR "loading"; if
-  // "loading", the loading overlay will be displayed on top of the paused overlay
-  const isPausedOverlayVisible = overlayState !== OverlayState.playing;
-  // Show the loading overlay only if the overlay state is specifically "loading"
-  const isLoadingOverlayVisibile = overlayState === OverlayState.loading;
+  const shouldShowLoadingOverlay = playbackState === 'LOADING';
+  const shouldShowPausedOverlay =
+    playbackState === 'PAUSED' || shouldShowLoadingOverlay;
 
   return (
     <div
@@ -175,10 +276,10 @@ const HoverVideoPlayer = ({
           style={{
             ...pausedOverlayWrapperSizingStyles[sizingMode],
             zIndex: 1,
-            opacity: isPausedOverlayVisible ? 1 : 0,
+            opacity: shouldShowPausedOverlay ? 1 : 0,
             transition: `opacity ${overlayTransitionDuration}ms`,
             // Disable pointer events on the paused overlay when it's hidden
-            pointerEvents: isPausedOverlayVisible ? 'auto' : 'none',
+            pointerEvents: shouldShowPausedOverlay ? 'auto' : 'none',
             ...pausedOverlayWrapperStyle,
           }}
           className={pausedOverlayWrapperClassName}
@@ -192,12 +293,12 @@ const HoverVideoPlayer = ({
           style={{
             ...expandToFillContainerStyle,
             zIndex: 2,
-            opacity: isLoadingOverlayVisibile ? 1 : 0,
+            opacity: shouldShowLoadingOverlay ? 1 : 0,
             transition: `opacity ${overlayTransitionDuration}ms ${
-              isLoadingOverlayVisibile ? loadingStateTimeout : 0
+              shouldShowLoadingOverlay ? loadingStateTimeout : 0
             }ms`,
             // Disable pointer events on the loading overlay when it's hidden
-            pointerEvents: isLoadingOverlayVisibile ? 'auto' : 'none',
+            pointerEvents: shouldShowLoadingOverlay ? 'auto' : 'none',
             ...loadingOverlayWrapperStyle,
           }}
           className={loadingOverlayWrapperClassName}
@@ -212,10 +313,10 @@ const HoverVideoPlayer = ({
             ...expandToFillContainerStyle,
             zIndex: 3,
             // Show the hover overlay when the player is hovered/playing
-            opacity: shouldPlayVideo ? 1 : 0,
+            opacity: !isHovering ? 1 : 0,
             transition: `opacity ${overlayTransitionDuration}ms`,
             // Disable pointer events on the hover overlay when it's hidden
-            pointerEvents: shouldPlayVideo ? 'auto' : 'none',
+            pointerEvents: !isHovering ? 'auto' : 'none',
             ...hoverOverlayWrapperStyle,
           }}
           className={hoverOverlayWrapperClassName}
@@ -228,7 +329,7 @@ const HoverVideoPlayer = ({
       <video
         // If a playback range is set, the loop attribute will not work correctly so there's no point in setting it here;
         // in that case, we will manually implement this behavior
-        loop={loop && !hasPlaybackRange}
+        loop={loop}
         playsInline
         preload={preload}
         crossOrigin={crossOrigin}
@@ -243,6 +344,38 @@ const HoverVideoPlayer = ({
         className={videoClassName}
         id={videoId}
         data-testid="video-element"
+        onPlay={() => {
+          setPlaybackState('LOADING');
+        }}
+        onPlaying={() => {
+          setPlaybackState('PLAYING');
+        }}
+        onPause={() => {
+          if (isHovering && loop) {
+            const videoElement = videoRef.current;
+            const videoStartTime = playbackRangeStart || 0;
+            videoElement.currentTime = videoStartTime;
+            videoElement.play();
+          }
+        }}
+        onTimeUpdate={() => {
+          const videoElement = videoRef.current;
+
+          if (
+            playbackRangeEnd !== null &&
+            videoElement.currentTime > playbackRangeEnd
+          ) {
+            if (loop) {
+              videoElement.currentTime = playbackRangeStart || 0;
+              if (videoElement.paused) {
+                videoElement.play();
+              }
+            } else {
+              videoElement.pause();
+              videoElement.currentTime = playbackRangeEnd;
+            }
+          }
+        }}
       >
         {!shouldUnloadVideo &&
           // Only render sources for the video if it is not unloaded
